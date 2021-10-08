9b758292e4573fc77412b7826883940a7317ca9f
kasiaw <kasiawkasiaw1@gmail.com>
2021-10-08 16:38:16 +0200

    :js-suma-zmiennych

---
+++ main.js
@@ -0,0 +1,12 @@
+let firstNumber = 15;
+
+let secondNumber = 7;
+
+function add(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+
+    return result;
+
+    console.log(add(4,5));
+}
+
